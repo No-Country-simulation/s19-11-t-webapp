09aa03b38376b3f6af8775e0bf92279e7dbfbc24
David Castro <davidernesto.castroanaya@gmail.com>
2024-11-20 11:35:19 -0600

    Adding Homepage

---
+++ Healthtech/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,55 @@
+import { Navbar, Nav, Button, Container } from 'react-bootstrap';
+import './styles/navbar.css'
+
+function CustomNavbar() {
+  return (
+    <Navbar bg="light" expand="lg" className="shadow-sm">
+      <Container>
+        {/* Logo */}
+        <Navbar.Brand href="#">
+          <img
+            src="./src/assets/img/navbar/carenet_logo.png"
+            alt="CareNet"
+            height="40"
+          />
+        </Navbar.Brand>
+
+        {/* Toggler for mobile view */}
+        <Navbar.Toggle aria-controls="navbar-nav" />
+
+        {/* Navbar links */}
+        <Navbar.Collapse id="navbar-nav">
+          <Nav className="mx-auto">
+            <Nav.Link href="#" className="text-dark fw-bold">
+              Home
+            </Nav.Link>
+            <Nav.Link href="#" className="text-dark fw-bold">
+              Service
+            </Nav.Link>
+            <Nav.Link href="#" className="text-dark fw-bold">
+              Contact Us
+            </Nav.Link>
+            <Nav.Link href="#" className="text-dark fw-bold">
+              Help
+            </Nav.Link>
+            <Nav.Link href="#" className="text-dark fw-bold">
+              Blog
+            </Nav.Link>
+          </Nav>
+
+          {/* Buttons */}
+          <div className="d-flex align-items-center">
+            <Nav.Link href="#" className="text-primary fw-bold me-3">
+              Sign Up
+            </Nav.Link>
+            <Button variant="primary" className="fw-bold btn-login">
+              Log In
+            </Button>
+          </div>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default CustomNavbar;
