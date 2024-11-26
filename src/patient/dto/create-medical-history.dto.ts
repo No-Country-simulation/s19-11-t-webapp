4ff3160665c3947dbcdf73883dc220acc535b795
Leandro Sánchez <103838718+leanmsan@users.noreply.github.com>
2024-11-26 16:00:09 -0300

    Revert "Medical record service"

--- src/patient/dto/create-medical-history.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { IsString, IsNotEmpty, IsArray } from "class-validator";
-
-export class CreatePatientDto {
-  @ApiProperty({ description: "Unique patient identifier" })
-  @IsString()
-  @IsNotEmpty()
-  patientId: string;
-}
