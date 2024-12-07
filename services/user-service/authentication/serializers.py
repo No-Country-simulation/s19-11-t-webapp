0e8226289b0ea41c9353da520221898f7f662205
Leandro Sánchez <leanmsan@gmail.com>
2024-12-07 03:41:28 -0300

    Cambio de JWT a SessionId
    Signed-off-by: Leandro Sánchez <leanmsan@gmail.com>

--- services/user-service/authentication/serializers.py
+++ services/user-service/authentication/serializers.py
@@ -1,48 +1,18 @@
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-from django.contrib.auth import authenticate
 from users.models import Usuario, Paciente, Medico
 from rest_framework import serializers
 
+class CustomUserSerializer(serializers.ModelSerializer):
+    user_type = serializers.SerializerMethodField()
 
-class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     class Meta:
-         model = Usuario
-         # Solo los campos que deseas exponer
-         fields = ['email', 'name', 'last_name', 'user_type']
+        model = Usuario
+        fields = ['email', 'first_name', 'last_name', 'user_type']
 
-    def validate(self, attrs):
-        email = attrs.get('email')
-        password = attrs.get('password')
-
-         # Intentamos autenticar al usuario
-        user = authenticate(request=self.context.get('request'), username=email, password=password)
-
-        if user is None:
-           raise serializers.ValidationError('Invalid credentials')
-        
-        if not user.is_active:
-            raise serializers.ValidationError('Account is inactive')
-
-        # Determinar el tipo de usuario, si es Paciente, Medico o Administrador
-        if user.is_staff:
-            user_type = 'Administrador'
-
-        if Paciente.objects.filter(id_usuario=user).exists():
-             user_type = 'Paciente'
-
-        if Medico.objects.filter(id_usuario=user).exists():
-            user_type = 'Medico'
-
-        attrs['user'] = user
-
-        # Generar el token         
-        data = super().validate(attrs)
-
-        # Incluir información adicional en la respuesta
-        data['user'] = {
-                'name': user.first_name,
-                'last_name': user.last_name,
-                'user_type': user_type
-            }
-
-        return data
+    def get_user_type(self, obj):
+        if obj.is_staff:
+            return 'Administrador'
+        if Paciente.objects.filter(id_usuario=obj).exists():
+            return 'Paciente'
+        if Medico.objects.filter(id_usuario=obj).exists():
+            return 'Medico'
+        return 'Usuario'
